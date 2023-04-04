19da7a41ec047dd0cccbec0d99d55246d9ce2b4a
crossK <crossk.noisyworks@gmail.com>
2023-04-05 01:58:13 +0900

    [update, modify] Storybook7にアップグレード、assets/publicディレクトリの設定を追加

--- nuxt-sample_v3/.storybook/main.js
+++ nuxt-sample_v3/.storybook/main.js
@@ -1,41 +1,43 @@
-const { mergeConfig } = require('vite')
-
+const path = require('path');
+const {
+  mergeConfig
+} = require('vite');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss')
+      }
     }
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  }, "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/vue3-vite",
+    options: {}
   },
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: async (config) => {
+  "staticDirs": [
+    { from: "../src/assets", to: "assets"},
+    { from: "../public", to: "./"},
+  ],
+  viteFinal: async config => {
+    config.resolve.alias['@'] = path.resolve(__dirname, "../src");
     return mergeConfig(config, {
       css: {
         preprocessorOptions: {
           scss: {
             additionalData: `
             @use "../src/assets/styles/global/_index.scss" as *;
-              `,
-          },
-        },
-      },
-    })
+              `
+          }
+        }
+      }
+    });
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
